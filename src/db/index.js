54995e5224739a7782342f0c97f45ea3a99b15ac
om dwivedi <dwivediomacc2@gmail.com>
2023-12-28 18:58:13 +0530

    Connection to the mongoDB

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+export const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log('Connection SUCCESS: mongoDB connected: ', connectionInstance.connection.host)
+    } catch (error) {
+        console.log('Connection FAILED: ', error);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+
+dotenv.config({ path: "../.env" });
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;( async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         console.log("connection SUCCESS Host: ", connectionInstance.connection.host );
+//     } catch (error) {
+//         console.error("Connection FAILED: ", error);
+//         process.exit(1)
+//     }
+// })()
